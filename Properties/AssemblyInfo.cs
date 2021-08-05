f4dae82eab8145f953d7b29ad9babdf54b3fd458
TuxPuf <radutux13@gmail.com>
2021-08-05 17:56:22 +0300

    Added more info + Updated version number

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MirrorFakePerson")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Mirror Fake Person")]
+[assembly: AssemblyDescription("Half-mirrors an AI generated fake person")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MirrorFakePerson")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCompany("TuxPuf Enterprises")]
+[assembly: AssemblyProduct("Mirror Fake Person")]
+[assembly: AssemblyCopyright("Copyright © TuxPuf Enterprises 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
