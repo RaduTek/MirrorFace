fe63bbf84fa6ba13e41e6638f6ed0de34565f52d
TuxPuf <radutux13@gmail.com>
2021-08-05 17:56:37 +0300

    Added About dialog

---
+++ AboutDialog.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace MirrorFakePerson
+{
+    partial class AboutDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.closeButton = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.versionLabel = new System.Windows.Forms.Label();
+            this.githubLink = new System.Windows.Forms.LinkLabel();
+            this.noteLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeButton.Location = new System.Drawing.Point(305, 122);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(80, 26);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.titleLabel.Font = new System.Drawing.Font("Segoe UI Light", 14F);
+            this.titleLabel.Location = new System.Drawing.Point(12, 12);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(371, 28);
+            this.titleLabel.TabIndex = 2;
+            this.titleLabel.Text = "Mirror Fake Person";
+            // 
+            // versionLabel
+            // 
+            this.versionLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.versionLabel.Location = new System.Drawing.Point(15, 43);
+            this.versionLabel.Name = "versionLabel";
+            this.versionLabel.Size = new System.Drawing.Size(368, 18);
+            this.versionLabel.TabIndex = 3;
+            this.versionLabel.Text = "Version";
+            // 
+            // githubLink
+            // 
+            this.githubLink.ActiveLinkColor = System.Drawing.SystemColors.HotTrack;
+            this.githubLink.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.githubLink.AutoSize = true;
+            this.githubLink.DisabledLinkColor = System.Drawing.SystemColors.GrayText;
+            this.githubLink.LinkColor = System.Drawing.SystemColors.Highlight;
+            this.githubLink.Location = new System.Drawing.Point(15, 128);
+            this.githubLink.Name = "githubLink";
+            this.githubLink.Size = new System.Drawing.Size(104, 15);
+            this.githubLink.TabIndex = 5;
+            this.githubLink.TabStop = true;
+            this.githubLink.Text = "GitHub Repository";
+            this.githubLink.VisitedLinkColor = System.Drawing.SystemColors.Highlight;
+            this.githubLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // noteLabel
+            // 
+            this.noteLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.noteLabel.Location = new System.Drawing.Point(15, 64);
+            this.noteLabel.Name = "noteLabel";
+            this.noteLabel.Size = new System.Drawing.Size(368, 48);
+            this.noteLabel.TabIndex = 6;
+            this.noteLabel.Text = "This application is released as open source, do whatever you want with it, I don\'" +
+    "t care. Maybe credit me for the smart idea of making this thing.";
+            // 
+            // AboutDialog
+            // 
+            this.AcceptButton = this.closeButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(397, 160);
+            this.Controls.Add(this.noteLabel);
+            this.Controls.Add(this.githubLink);
+            this.Controls.Add(this.versionLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.closeButton);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Load += new System.EventHandler(this.AboutDialog_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label versionLabel;
+        private System.Windows.Forms.LinkLabel githubLink;
+        private System.Windows.Forms.Label noteLabel;
+    }
+}
---
+++ AboutDialog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MirrorFakePerson
+{
+    public partial class AboutDialog : Form
+    {
+        public AboutDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/TuxPuf/MirrorFakePerson");
+        }
+
+        private void AboutDialog_Load(object sender, EventArgs e)
+        {
+            versionLabel.Text = "Version " + Application.ProductVersion;
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
