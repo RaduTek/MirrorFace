f8e806f734e5a694496ae3107d5f71b59e9f2b5f
RaduTek <radutux13@gmail.com>
2021-09-09 19:31:59 +0300

    Update version number

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Mirror Fake Person")]
 [assembly: AssemblyDescription("Half-mirrors an AI generated fake person")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("TuxPuf Enterprises")]
+[assembly: AssemblyCompany("RaduTek")]
 [assembly: AssemblyProduct("Mirror Fake Person")]
-[assembly: AssemblyCopyright("Copyright © TuxPuf Enterprises 2021")]
+[assembly: AssemblyCopyright("Copyright © RaduTek 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
